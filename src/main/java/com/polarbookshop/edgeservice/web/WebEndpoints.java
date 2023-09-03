02c11edd1edc5ceb8a089590dc6b003e828dd49c
azimbabu <azim.babu@gmail.com>
2023-09-03 00:24:15 -0700

    add urls and logs

--- src/main/java/com/polarbookshop/edgeservice/web/WebEndpoints.java
+++ src/main/java/com/polarbookshop/edgeservice/web/WebEndpoints.java
@@ -1,5 +1,7 @@
 package com.polarbookshop.edgeservice.web;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpStatus;
@@ -11,12 +13,20 @@ import reactor.core.publisher.Mono;
 @Configuration
 public class WebEndpoints {
 
+  private static final Logger log = LoggerFactory.getLogger(WebEndpoints.class);
+
   @Bean
   public RouterFunction<ServerResponse> routerFunction() {
     return RouterFunctions.route()
-        .GET("/catalog-fallback", request -> ServerResponse.ok().body(Mono.just(""), String.class))
+        .GET("/catalog-fallback", request -> {
+          log.info("Using GET catalog-fallback");
+          return ServerResponse.ok().body(Mono.just(""), String.class);
+        })
         .POST("/catalog-fallback",
-            request -> ServerResponse.status(HttpStatus.SERVICE_UNAVAILABLE).build())
+            request -> {
+              log.info("Using POST catalog-fallback");
+              return ServerResponse.status(HttpStatus.SERVICE_UNAVAILABLE).build();
+            })
         .build();
   }
 }
